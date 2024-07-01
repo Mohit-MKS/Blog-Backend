942bd76554a32fa6aec938649158bd2a89642b6e
Mohit <mksagar111@gmail.com>
2024-07-01 23:15:15 +0530

    compile error fix

--- src/validators/errorValidator.ts
+++ src/validators/errorValidator.ts
@@ -1,18 +1,21 @@
 import { validationResult } from "express-validator";
+import { ApiRequest } from "../models/interfaces/requests.interface";
+import { ApiResponse } from "../models/interfaces/response.interface";
+import { NextFunction } from "express";
 
-const validateErrors = (req, res, next) => {
-    const errors = validationResult(req);
-    const errorsObj = {}
-    if (errors.isEmpty())
-        return next();
+const validateErrors = (req: ApiRequest, res: ApiResponse, next: NextFunction) => {
+  const errors = validationResult(req);
+  const errorsObj: any = {}
+  if (errors.isEmpty())
+    return next();
 
 
-    errors.array().map((error: any) => {
-        if (!errorsObj[error.path])
-            errorsObj[error.path] = error.msg;
-    })
+  errors.array().map((error: any) => {
+    if (!errorsObj[error.path])
+      errorsObj[error.path] = error.msg;
+  })
 
-    return res.status(400).json({ message: errorsObj })
+  return res.status(400).json({ message: errorsObj })
 }
 
 export { validateErrors }
