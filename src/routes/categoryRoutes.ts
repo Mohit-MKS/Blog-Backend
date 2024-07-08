ddbfa45a63550f97f5c76d22bde1e108b26e5d87
Mohit Kumar <mksagar111@gmail.com>
2024-07-08 06:45:24 +0000

    get category api added

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -9,5 +9,7 @@ router.get("/", isAuth, isAdmin, categoryController.getCategories)
 router.post("/", isAuth, isAdmin, categoryValidators.validateAddCategory, categoryController.addCategory)
 router.put("/:categoryId", isAuth, isAdmin, categoryController.updateCategory)
 router.delete("/:categoryId", isAuth, isAdmin, categoryController.deleteCategory)
+router.get("/:categoryId", isAuth, isAdmin, categoryController.getCategory)
+
 
 export = router
