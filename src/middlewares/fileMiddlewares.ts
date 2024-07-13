1ed6d53e7ec244ff99f63ce8f8ffdcc106ad9b4d
Mohit <mksagar111@gmail.com>
2024-07-13 23:31:31 +0530

    grid-fs compatiblity

--- src/middlewares/fileMiddlewares.ts
+++ src/middlewares/fileMiddlewares.ts
@@ -9,26 +9,23 @@ import { Request } from "express";
 const storage = new GridFsStorage({
   url: config.MONGO_CONNECTION_URL,
   cache: true,
+  options: { useUnifiedTopology: true },
   file: (req, file) => {
-    if (Constants.AllowedFileTypes.includes(file.mimetype)) {
-      const oName = file.originalname;
-      const extension = path.extname(oName);
-      const fName = oName.replace(extension, '');
-      const fNameFormated = fName.split(' ').join('_').toLocaleLowerCase();
-      const code = generateCode(12);
-      const fNameToSave = `${fNameFormated}_${code}${extension}`;
-      return {
-        fileName: fNameToSave,
-        bucketName: 'files'
-      };
-    } else {
-      return null;
-    }
+    const oName = file.originalname;
+    const extension = path.extname(oName);
+    const fName = oName.replace(extension, '');
+    const fNameFormated = fName.split(' ').join('_').toLocaleLowerCase();
+    const code = generateCode(12);
+    const fNameToSave = `${fNameFormated}_${code}${extension}`;
+    const fileObj = {
+      fileName: fNameToSave,
+      bucketName: 'files'
+    };
+    return fileObj
   }
 });
 
 const fileFilter = (req: Request, file: Express.Multer.File, callback: FileFilterCallback) => {
-  callback(null, true)
   const mimetype = file.mimetype;
   if (Constants.AllowedFileTypes.includes(mimetype)) {
     callback(null, true)
@@ -40,7 +37,7 @@ const fileFilter = (req: Request, file: Express.Multer.File, callback: FileFilte
 
 const upload = multer({
   fileFilter,
-  limits: { fieldSize: 1024 * 1024 },
+  limits: { fieldSize: 1024 * 1024 * 1024 },
   storage,
 })
 
