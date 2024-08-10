bbb7b14dbea60a69e706b67fc761e51a28bb3162
Mohit <mksagar111@gmail.com>
2024-08-10 16:08:40 +0530

    post detail api added

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -8,7 +8,8 @@ import { upload } from "../middlewares/fileMiddlewares";
 const router = Router();
 router.post('/', isAuth, upload.single('file'), postValidators.validatePost, postController.addPost)
 router.get('/', isAuth, postController.getPosts)
+router.get('/:postId', isAuth, postValidators.validatePostId, postController.getPostDetails)
 router.put('/:postId', isAuth, upload.single('file'), postValidators.validateUpdatePost, postController.updatePost)
-router.delete('/:postId', isAuth, postValidators.validateDeletePost, postController.deletePost)
+router.delete('/:postId', isAuth, postValidators.validatePostId, postController.deletePost)
 
 export = router;
